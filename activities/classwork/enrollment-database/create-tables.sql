fe670db634b4967f3f604b58d27f69e7e18ce70e
alissonochoa <ali.och.amd@gmail.com>
2019-10-25 19:53:01 -0500

    Classwork

--- activities/classwork/enrollment-database/create-tables.sql
+++ activities/classwork/enrollment-database/create-tables.sql
@@ -1,16 +1,16 @@
 -- Create student table
 CREATE TABLE IF NOT EXISTS student (
-    id UUID,
+    id UUID, -- es un tipo de ID aleatorio
     first_name TEXT,
     last_name TEXT,
     gender TEXT,
     gpa NUMERIC,
     age INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id)  -- tiene que ser unica en la tabla
 );
 
 -- Create course table
-CREATE TABLE IF NOT EXISTS course (
+CREATE TABLE IF NOT EXISTS course ( --n a 1 (muchos estudiantes pueden asistir a un curso)
     id UUID,
     name TEXT,
     department TEXT,
@@ -23,9 +23,9 @@ CREATE TABLE IF NOT EXISTS course (
 -- Create enrollment table
 CREATE TABLE IF NOT EXISTS enrollment (
     id UUID,
-    student_id UUID REFERENCES student(id) ON DELETE CASCADE,
+    student_id UUID REFERENCES student(id) ON DELETE CASCADE, --si hay otra relación, se borrará todo por eso se borra en cascada
     course_id UUID REFERENCES course(id) ON DELETE CASCADE,
     application_date DATE,
-    approved INT,
+    approved INT,  -- son enteros pero se pueden usar boleanos
     PRIMARY KEY (id)
 );
--- activities/classwork/enrollment-database/exercise.sql
+++ activities/classwork/enrollment-database/exercise.sql
@@ -5,15 +5,59 @@
 
 
 -- A) Get all the unique department names.
+SELECT DISTINCT department FROM course;
 
 -- B) Get the top 10 female students (first_name, last_name, age, gpa) with the best GPA scores and order by age (asc).
+SELECT
+	first_name AS "First Name", -- "renombrar", 'cadenas de texto'
+	last_name AS "Last Name",
+	age AS "Age",
+	gpa AS "GPA"
+FROM
+	student
+WHERE
+	gender = 'female'
+ORDER BY
+	gpa DESC, --descendiente
+	age ASC,
+	first_name ASC,
+	last_name ASC
+LIMIT 10 -- solo 10 personas
+;
 
 -- C) Count the number of male/female students that are at least 25 years old.
+SELECT
+	gender AS "Gender",
+	count(gender) AS "Count" -- para que funciones tiene que haber un GROUP BY
+FROM
+	student
+WHERE
+	age >= 25
+GROUP BY gender
+;
 
 -- D) Get the number of male/female students that were accepted
-
+SELECT
+    gender,
+    count(gender)
+FROM
+    student INNER JOIN enrollment ON student.id = enrollment.student_id -- INNER JOIN = product cartesiano
+WHERE
+    enrollment.approved = 1
+GROUP BY
+    gender
+;
 -- E) Get the min, average, and max GPA of the accepted male students that are less than 20 years old.
-
+SELECT
+    MIN(gpa),
+    AVG(gpa),
+    MAX(gpa)
+FROM
+    student INNER JOIN enrollment ON student.id = enrollment.student_id
+WHERE
+    enrollment.approved = 1
+    AND student.gender = 'male'
+;
 -- F) Get the number of enrollments to courses that take longer than 2 years to finalize.
 
 -- G) Get the number of male/female student that will take a course from the 'Statistics' department.
